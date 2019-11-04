f635af9bb9b1565dbc4b2d18ef9e72ea92faf5c9
Alejandro Rayas <alexrayas@rocketmail.com>
2019-11-03 17:02:39 -0700

    finished version
    some things can be still tweaked by this a good base version

---
+++ index.js
@@ -0,0 +1,20 @@
+// // const superheroes = require('superheroes');
+// // const supervillains = require('supervillains');
+// var imported = document.createElement('script');
+// imported.src = 'bundle.js';
+// document.head.appendChild(imported);
+
+// //supervillains.all;
+// //=> ['Abattoir', 'Able Crown', …]
+
+// //superheroes.all;//=> ['3-D Man', 'A-Bomb', …]
+ 
+// var superheroName = superheroes.random();
+
+// var supervillanName = supervillains.random();
+// //=> 'Mud Pack'
+
+// $("button").on("click", function(){
+//     $(".fighters").text("blahhhh");
+// })
+// console.log(superheroName, "vs", supervillanName );
